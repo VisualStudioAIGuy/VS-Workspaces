80e49d6b27496fd140a914d1adec943656f37f8a
VisualStudioAIGuy <antonjo28@amityregion5.org>
2025-05-07 18:34:02 -0400

    Updating AI

--- Heart Failure.py
+++ Heart Failure.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-def calculate_percent(dataset, number):
-    total_count = dataset['HeartDisease'].shape[0]
-    zeros_count_heart_disease = (dataset['HeartDisease'] == number).sum()
+def calculate_percent(dataset, number, type="HeartDisease"):
+    total_count = dataset[type].shape[0]
+    zeros_count_heart_disease = (dataset[type] == number).sum()
     zeros_percentage_heart_disease = (zeros_count_heart_disease / total_count) * 100
-    print(f"Percentage of {number}s in 'HeartDisease' column: {zeros_percentage_heart_disease}%")
+    print(f"Percentage of {number}s in {type} column: {zeros_percentage_heart_disease}%")
 
 
 dataset = pd.read_csv('Datasets/heart.csv')
@@ -15,3 +15,6 @@ print(dataset.info())
 
 calculate_percent(dataset, 0)
 calculate_percent(dataset, 1)
+
+"""dataset_G = dataset.groupby("Age")
+print(dataset_G["HeartDisease"].mean().sort_values())"""
